34bb8d56bd375a788b101be2b6a8db0eb93cc4bc
ferdin448 <ferdin448@gmail.com>
2025-07-21 15:20:13 +0530

    Update  to version 3.12

--- lambda_compile.sh
+++ lambda_compile.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-yum install -y python37
-python3.7 -m venv /tmp/venv
+# Install Python 3.12 (on Amazon Linux 2023 or with correct repo for your distro)
+yum install -y python3.12
+
+# Create a virtual environment with Python 3.12
+python3.12 -m venv /tmp/venv
+
+# Upgrade pip and setuptools in the venv
 /tmp/venv/bin/pip install --upgrade pip setuptools
+
+# Install your package (editable mode)
 /tmp/venv/bin/pip install -e .
-cp -r /tmp/venv/lib/python3.7/site-packages/. ./aws_lambda_libs
-cp -r /tmp/venv/lib64/python3.7/site-packages/. ./aws_lambda_libs
+
+# Copy all dependencies to ./aws_lambda_libs
+cp -r /tmp/venv/lib/python3.12/site-packages/. ./aws_lambda_libs
+cp -r /tmp/venv/lib64/python3.12/site-packages/. ./aws_lambda_libs
