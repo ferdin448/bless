c9b132290d95d556d38964794f53c7f73fc3eacc
ferdin448 <ferdin448@gmail.com>
2025-07-22 12:03:09 +0530

    test

--- lambda_compile.sh
+++ lambda_compile.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-# Install Python 3.12 if needed
-yum install -y python312 python3.12-venv
-
-# Use Python 3.12 for venv
-python3.12 -m venv /tmp/venv
-. /tmp/venv/bin/activate
-
-# Upgrade pip and install dependencies
-pip install --upgrade pip setuptools
-pip install -e .
-
-# Copy site-packages to lambda folder
-mkdir -p ./aws_lambda_libs
-cp -r /tmp/venv/lib/python3.12/site-packages/. ./aws_lambda_libs
-cp -r /tmp/venv/lib64/python3.12/site-packages/. ./aws_lambda_libs || true
+python --version
+python3 --version
